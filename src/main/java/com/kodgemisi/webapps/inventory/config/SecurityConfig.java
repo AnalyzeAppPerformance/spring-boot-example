2f555ff5e001db372a63c02f3e28142e3bc35876
panczur <panczur@gmail.com>
2015-12-17 15:29:31 +0100

    JMeter plugin added to pom.xml, sample JMeter test added

--- src/main/java/com/kodgemisi/webapps/inventory/config/SecurityConfig.java
+++ src/main/java/com/kodgemisi/webapps/inventory/config/SecurityConfig.java
@@ -7,11 +7,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by sedat on 26.06.2015.
@@ -20,27 +18,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-//@EnableWebSecurity
 @EnableWebMvcSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                //.antMatchers("/jobs/create").authenticated()
                 .antMatchers("/register").permitAll()
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
+            .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error")
                 .usernameParameter("username")
                 .permitAll()
                 .and()
-                .logout()
+            .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .permitAll();
